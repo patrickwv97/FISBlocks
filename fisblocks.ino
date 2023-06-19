56c3ea1f6ed5240dcf5e31180feb3c2ceb22b226
patrick_wv <patrick.winkelvoss@outlook.de>
2023-06-19 14:37:30 +0200

    Update fisblocks.ino

--- fisblocks.ino
+++ fisblocks.ino
@@ -17,11 +17,13 @@ void setup(){
   Serial.begin(9600);
   for(int i=0; i<8; i++){
     LCD.showText("Willkommen");
-    delay(500);
+    delay(2000);
   }
 }
 
 void loop(){
-  LCD.showText("Hallo du", "KNECK!)
-  delay(500)
+  LCD.showText("Hallo du", "KNECK!);
+  delay(2000);
+  LCD.showText("Lecki", "Cocki");
+  delay(2000);
 }
